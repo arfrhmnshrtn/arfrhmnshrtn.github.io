494d77263cf787e1d049a25603c728ef41128220
arfrhmnshrtn <iw8628940@gmail.com>
2021-03-05 15:23:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 10){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu ').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
